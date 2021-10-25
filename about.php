0a759f5c6509d9fc84ba65eb8f1c6c99fd2a1121
wowdehs <93096761+wowdehs@users.noreply.github.com>
2021-10-25 22:26:08 +0300

    Update about.php

--- about.php
+++ about.php
@@ -5,7 +5,7 @@
         <div class="row">
 
           <div class="col-lg-5 align-items-stretch video-box" style='background-image: url("assets/img/about.jpg");'>
-            <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
+            <a href="https://www.instagram.com/p/BaJBdQgFKuk/" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
           </div>
 
           <div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch">
